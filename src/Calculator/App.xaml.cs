616af0592587b719c9bf510df04c5c0305e35af3
Supraja130798 <sxboppana4@shockers.wichita.edu>
2023-02-25 04:00:16 +0700

    Update and Config

--- src/Calculator/App.xaml.cs
+++ src/Calculator/App.xaml.cs
@@ -2,10 +2,14 @@
 
 public partial class App : Application
 {
-	public App()
+	public static Color pageBgColor = null;
+	public static Color lblColor = null;
+	public static Color navColor = null;
+
+    public App()
 	{
 		InitializeComponent();
-
-		MainPage = new MainPage();
+		//MainPage = new MainPage();
+		MainPage = new AppFlyout();
 	}
 }
