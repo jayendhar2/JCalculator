c5fb4aa736964512ba03624c1b1e5f98813c15c6
Supraja130798 <sxboppana4@shockers.wichita.edu>
2023-02-24 14:46:07 +0700

    Add Advanced Page

--- src/Calculator/AdvancedPage.xaml.cs
+++ src/Calculator/AdvancedPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace Calculator;
 
 public partial class AdvancedPage : ContentPage
@@ -39,16 +41,26 @@ public partial class AdvancedPage : ContentPage
         }
 
         this.resultText.Text += pressed;
+
+        this.CurrentCalculation.Text += pressed;
+
     }
 
     void OnSelectOperator(object sender, EventArgs e)
     {
         LockNumberValue(resultText.Text);
+        OnCalculate(this, null);
 
         currentState = -2;
         Button button = (Button)sender;
         string pressed = button.Text;
+        if (pressed == "÷")
+        {
+            decimalFormat = "N2";
+        }
         mathOperator = pressed;
+
+        this.CurrentCalculation.Text += pressed;
     }
 
     private void LockNumberValue(string text)
@@ -77,6 +89,7 @@ public partial class AdvancedPage : ContentPage
         decimalFormat = "N0";
         this.resultText.Text = "0";
         currentEntry = string.Empty;
+        CurrentCalculation.Text = "";
     }
 
     void OnCalculate(object sender, EventArgs e)
@@ -88,25 +101,54 @@ public partial class AdvancedPage : ContentPage
 
             double result = Calculator.Calculate(firstNumber, secondNumber, mathOperator);
 
-            this.CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
+            //this.CurrentCalculation.Text = $"{firstNumber} {mathOperator} {secondNumber}";
 
             this.resultText.Text = result.ToTrimmedString(decimalFormat);
             firstNumber = result;
             secondNumber = 0;
-            currentState = -1;
+            currentState = 1;
             currentEntry = string.Empty;
+        }
+    }
+
+    void OnCalculateResult(object sender, EventArgs e)
+    {
+        string express = CurrentCalculation.Text;
+        if (express.Contains("("))
+        {
+            try
+            {
+                express = express.Replace("×", "*").Replace("÷", "/").Replace("Mod", "%");
+                string res = new DataTable().Compute(express, null).ToString();
+                resultText.Text = res;
+                CurrentCalculation.Text = "";
+            }
+            catch (Exception ex)
+            {
+                resultText.Text = "Invalid";
+                CurrentCalculation.Text = "";
+            }
+            
+        }
+        else
+        {
+            OnCalculate(sender, e);
+            CurrentCalculation.Text = "";
         }
     }
 
     void OnNegative(object sender, EventArgs e)
     {
-        if (currentState == 1)
-        {
-            secondNumber = -1;
-            mathOperator = "×";
-            currentState = 2;
-            OnCalculate(this, null);
-        }
+        //if (currentState == 1)
+        //{
+        //    secondNumber = -1;
+        //    mathOperator = "×";
+        //    currentState = 2;
+        //    OnCalculate(this, null);
+        //}
+        double number;
+        double.TryParse(resultText.Text, out number);
+        resultText.Text = (number * (-1)).ToTrimmedString(decimalFormat);
     }
 
     void OnPercentage(object sender, EventArgs e)
@@ -119,6 +161,32 @@ public partial class AdvancedPage : ContentPage
             mathOperator = "×";
             currentState = 2;
             OnCalculate(this, null);
+        }
+    }
+
+    void OnSelectRootOperator(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        string pressed = button.Text;
+        double number;
+        decimalFormat = "N2";
+        double.TryParse(resultText.Text, out number);
+        if (number >= 0)
+        {
+            try
+            {
+                CurrentCalculation.Text = CurrentCalculation.Text.Remove(CurrentCalculation.Text.Length - resultText.Text.Length) + (pressed + resultText.Text);
+                resultText.Text = Math.Sqrt(number).ToTrimmedString(decimalFormat);
+            }
+            catch (Exception ex)
+            {
+                resultText.Text = Math.Sqrt(number).ToTrimmedString(decimalFormat);
+            }
+
+        }
+        else
+        {
+            resultText.Text = "Invalid";
         }
     }
 }
